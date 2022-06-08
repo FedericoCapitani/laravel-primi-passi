5e5bd71f7cd8a4d60dd1c8ce01c58836bfae6c83
FedericoCapitani <federico.capitani@outlook.it>
2022-06-08 15:15:07 +0200

    add variables in home route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>primi passi</title>
+
+    <style>
+        .title{
+            text-align: center;
+        }
+        .subjects{
+            text-align: center;
+            list-style: none;
+        }
+    </style>
 </head>
+
 <body>
     
-    <h1>Hello World!</h1>
+    <h1 class="title">Hello World! from {{ $name }} </h1>
+
+    <ul class="subjects">
+        @foreach ($subjects as $subject)
+        <li> {{$subject}} </li>
+        @endforeach
+    </ul>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $name = 'Pippo';
+    $subjects = ['Learn PHP','Learn Laravel','Learn Js','Learn VueJs','Learn React'];
+    return view('home', compact('name','subjects'));
 });
